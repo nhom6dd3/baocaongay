7ff4e061aa8cf77545c03ddb7388d6f134104c1d
Hiển Minh <33273189+PhamMinhHien@users.noreply.github.com>
2018-12-10 10:40:22 +0700

    Tim hieu - Phú

---
+++ TimHieuDemo_NguyenHuuPhu/DemoProject/index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import Style from './Style';
+import Flexbox from './Flexbox';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Flexbox);
--- Webservice_Php (Login_Signin)/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $server_name = "localhost";
-    $user_name = "root";
-    $password = "";
-    $db = "qlsinhvien";
